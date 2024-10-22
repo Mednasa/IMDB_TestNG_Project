c89a2e2e17a94c205e71f116c20e4f6bc6ee8200
MertOzmen <avmertozmen93@gmail.com>
2024-10-22 15:53:59 +0300

    AdvancedSearch class has been created

---
+++ src/login_process/AdvancedSearch.java
@@ -0,0 +1,26 @@
+package login_process;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utility.BaseDriver;
+import utility.ConfigReader;
+
+public class AdvancedSearch extends BaseDriver {
+
+    @BeforeClass
+    public void Login(){
+
+        o_imdb.myClick(o_imdb.SignInBtn);
+        o_imdb.myClick(o_imdb.SignInWithIMDb);
+        o_imdb.mySendKeys(o_imdb.emailPlc, ConfigReader.getProperty("email"));
+        o_imdb.mySendKeys(o_imdb.passwordPlc, ConfigReader.getProperty("password"));
+        o_imdb.myClick(o_imdb.SignIn);
+
+    }
+
+    @Test
+    public void AdvancedSearch(){
+        m_imdb.myClick(m_imdb.AllBtn);
+
+    }
+}
--- src/utility/Mert_Elements.java
+++ src/utility/Mert_Elements.java
@@ -1,5 +1,7 @@
 package utility;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Mert_Elements extends ParentPage {
@@ -7,4 +9,8 @@ public class Mert_Elements extends ParentPage {
     public Mert_Elements() {
         PageFactory.initElements(BaseDriver.driver, this);
     }
+
+    @FindBy(xpath = "(//*[@class='ipc-icon ipc-icon--arrow-drop-down ipc-btn__icon ipc-btn__icon--post searchCatSelector-button-post-icon'])[1]")
+    public WebElement AllBtn;
+
 }
