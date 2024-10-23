207732b7b5e6794fe12647b6641cf762a58c8b6a
Degisiklikler1 <megaozi9517536@gmail.com>
2024-10-23 19:49:06 +0300

    All assertion locators found and run.  Recently viewed section locaters found and run. Project Completed

--- src/login_process/HamburgerMenu.java
+++ src/login_process/HamburgerMenu.java
@@ -1,6 +1,7 @@
 package login_process;
 
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utility.BaseDriver;
@@ -25,124 +26,159 @@ public class HamburgerMenu extends BaseDriver {
     @Test
     public void hamburgerMenu() {
 
-        // Movies
+//         Movies
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myClick(oz_imdb.releaseCalendarButton);
+        oz_imdb.verifyContainsText(oz_imdb.releaseCalendarButtonAssert, "Upcoming releases");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.top250MoviesButton);
+        oz_imdb.verifyContainsText(oz_imdb.top250MoviesButtonAssert, "IMDb Top 250 Movies");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.mostPopularMoviesButton);
+        oz_imdb.verifyContainsText(oz_imdb.mostPopularMoviesButtonAssert, "Most Popular Movies");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.browseMoviesbyGenreButton);
+        oz_imdb.verifyContainsText(oz_imdb.browseMoviesbyGenreButtonAssert, "Popular interests");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.topBoxOfficeButton);
+        oz_imdb.verifyContainsText(oz_imdb.topBoxOfficeButtonAssert, "Top Box Office (US)");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.showtimesTicketsButtonButton);
+        oz_imdb.verifyContainsText(oz_imdb.showtimesTicketsButtonButtonAssert, "Showtimes");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.movieNewsButton);
+        oz_imdb.verifyContainsText(oz_imdb.movieNewsButtonAssert, "Movie News");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.indiaMovieSpotlightButton);
+        oz_imdb.verifyContainsText(oz_imdb.indiaMovieSpotlightButtonAssert, "Top Rated India Movies");
 
-        // TV Shows
+//         TV Shows
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.whatsonTVStreamingButton);
+        oz_imdb.verifyContainsText(oz_imdb.whatsonTVStreamingButtonAssert, "WHAT'S ON TV AND STREAMING");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.top250TVShowsButton);
+        oz_imdb.verifyContainsText(oz_imdb.top250TVShowsButtonAssert, "Top 250 TV Shows");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.mostPopularTVShowsButton);
+        oz_imdb.verifyContainsText(oz_imdb.mostPopularTVShowsButtonAssert, "Most Popular TV Shows");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.browseTVShowsbyGenreButton);
+        oz_imdb.verifyContainsText(oz_imdb.browseTVShowsbyGenreButtonAssert, "Popular interests");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.tVNewsButton);
+        oz_imdb.verifyContainsText(oz_imdb.tVNewsButtonAssert, "TV News");
 
-        // Awards & Events
+//         Awards & Events
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.oscarsButton);
+        oz_imdb.verifyContainsText(oz_imdb.oscarsButtonAssert, "Oscars");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.halloweenButton);
+        oz_imdb.verifyContainsText(oz_imdb.halloweenButtonAssert, "Scary Good");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.hispanicHeritageMonthButton);
+        oz_imdb.verifyContainsText(oz_imdb.hispanicHeritageMonthButtonAssert, "IMDb SPOTLIGHT");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.mamiButton);
+        oz_imdb.verifyContainsText(oz_imdb.mamiButtonAssert, "MAMI Mumbai Film Festival");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.sTARmeterAwardsButton);
+        oz_imdb.verifyContainsText(oz_imdb.sTARmeterAwardsButtonAssert, "IMDb STARmeter AWARDS");
+        BaseDriver.driver.navigate().back();
+        oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.awardsCentralButton);
+        oz_imdb.verifyContainsText(oz_imdb.awardsCentralButtonAssert, "AWARDS CENTRAL");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.festivalCentralButton);
+        oz_imdb.verifyContainsText(oz_imdb.festivalCentralButtonAssert, "FESTIVAL CENTRAL");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.allEventsButton);
+        oz_imdb.verifyContainsText(oz_imdb.awardsCentralButtonAssert, "All events");
 
-        // Celebs
+//         Celebs
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.bornTodayButton);
+        oz_imdb.verifyContainsText(oz_imdb.bornTodayButtonAssert, "Advanced name search");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.mostPopularCelebsButton);
+        oz_imdb.verifyContainsText(oz_imdb.mostPopularCelebsButtonAssert, "Most Popular Celebs");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.celebrityNewsButton);
+        oz_imdb.verifyContainsText(oz_imdb.celebrityNewsButtonAssert, "Celebrity News");
 
-        // Watch
+//         Watch
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.whattoWatchButton);
+        oz_imdb.verifyContainsText(oz_imdb.whattoWatchButtonAssert, "What to Watch - IMDb");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.latestTrailersButton);
+        oz_imdb.verifyContainsText(oz_imdb.latestTrailersButtonAssert, "Watch New Movie & TV Trailers");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.iMDbOriginalsButton);
+        oz_imdb.verifyContainsText(oz_imdb.iMDbOriginalsButtonAssert, "IMDb ORIGINALS");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.iMDbPicksButton);
+        oz_imdb.verifyContainsText(oz_imdb.iMDbPicksButtonAssert, "IMDb PICKS");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.iMDbSpotlightButton);
+        oz_imdb.verifyContainsText(oz_imdb.iMDbSpotlightButtonAssert, "IMDb SPOTLIGHT");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.iMDbPodcastsButton);
+        oz_imdb.verifyContainsText(oz_imdb.iMDbPicksButtonAssert, "Advanced title search");
 
-        // Community
+//        // Community
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.helpCenterButton);
+        oz_imdb.verifyContainsText(oz_imdb.helpCenterButtonAssert, "Help Center");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.contributorZoneButton);
+        oz_imdb.verifyContainsText(oz_imdb.contributorZoneButtonAssert, "What is a Contributor?");
         BaseDriver.driver.navigate().back();
         oz_imdb.myClick(oz_imdb.hamburgerMenuButton);
         oz_imdb.myJsClick(oz_imdb.pollsButton);
+        oz_imdb.verifyContainsText(oz_imdb.pollsButtonAssert, "IMDb Polls");
 
-        // Recently viewed
-
-
-
-
-
-
-
-
-
+//         Recently viewed
+        BaseDriver.driver.navigate().back();
+        oz_imdb.mySendKeys(oz_imdb.searchBox, "Star Wars Empire Strikes Back");
+        oz_imdb.myJsClick(oz_imdb.listBox);
+        oz_imdb.verifyContainsText(oz_imdb.starWarsAssert, "Yıldız Savaşları: İmparator");
+        BaseDriver.driver.navigate().back();
+        oz_imdb.scrollToElement(oz_imdb.addRecentlyWatchlist);
+        oz_imdb.addRecentlyWatchlist.isDisplayed();
+        oz_imdb.myJsClick(oz_imdb.addRecentlyWatchlist);
+        oz_imdb.removeRecentlyWatchlist.isDisplayed();
 
 
 
--- src/utility/Oguzhan_Elements.java
+++ src/utility/Oguzhan_Elements.java
@@ -52,70 +52,141 @@ public class Oguzhan_Elements extends ParentPage {
     public WebElement hamburgerMenuButton;
     @FindBy(linkText = "Release Calendar")
     public WebElement releaseCalendarButton;
-    @FindBy(linkText = "Release Calendar")
+    @FindBy(xpath = "(//span[text()='Upcoming releases'])")
+    public WebElement releaseCalendarButtonAssert;
+    @FindBy(linkText = "Top 250 Movies")
     public WebElement top250MoviesButton;
+    @FindBy(css = "[class='ipc-title__text chart-layout-specific-title-text']")
+    public WebElement top250MoviesButtonAssert;
     @FindBy(linkText = "Most Popular Movies")
     public WebElement mostPopularMoviesButton;
+    @FindBy(css = "[class='ipc-title__text chart-layout-specific-title-text']")
+    public WebElement mostPopularMoviesButtonAssert;
     @FindBy(linkText = "Browse Movies by Genre")
     public WebElement browseMoviesbyGenreButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement browseMoviesbyGenreButtonAssert;
     @FindBy(linkText = "Top Box Office")
     public WebElement topBoxOfficeButton;
+    @FindBy(css = "[class='ipc-title__text chart-layout-specific-title-text']")
+    public WebElement topBoxOfficeButtonAssert;
     @FindBy(linkText = "Showtimes & Tickets")
     public WebElement showtimesTicketsButtonButton;
+    @FindBy(linkText = "Showtimes")
+    public WebElement showtimesTicketsButtonButtonAssert;
     @FindBy(linkText = "Movie News")
     public WebElement movieNewsButton;
+    @FindBy(css = "[id='news-card-section']")
+    public WebElement movieNewsButtonAssert;
     @FindBy(linkText = "India Movie Spotlight")
     public WebElement indiaMovieSpotlightButton;
+    @FindBy(css = "[class='ipc-title ipc-title--base ipc-title--subsection-title ipc-title--on-textPrimary'] h3")
+    public WebElement indiaMovieSpotlightButtonAssert;
     @FindBy(linkText = "What's on TV & Streaming")
     public WebElement whatsonTVStreamingButton;
+    @FindBy(linkText = "WHAT'S ON TV AND STREAMING")
+    public WebElement whatsonTVStreamingButtonAssert;
     @FindBy(linkText = "Top 250 TV Shows")
     public WebElement top250TVShowsButton;
+    @FindBy(css = "[class='ipc-title__text chart-layout-specific-title-text']")
+    public WebElement top250TVShowsButtonAssert;
     @FindBy(linkText = "Most Popular TV Shows")
     public WebElement mostPopularTVShowsButton;
+    @FindBy(css = "[class='ipc-title__text chart-layout-specific-title-text']")
+    public WebElement mostPopularTVShowsButtonAssert;
     @FindBy(linkText = "Browse TV Shows by Genre")
     public WebElement browseTVShowsbyGenreButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement browseTVShowsbyGenreButtonAssert;
     @FindBy(linkText = "TV News")
     public WebElement tVNewsButton;
+    @FindBy(css = "[id='news-card-section']")
+    public WebElement tVNewsButtonAssert;
     @FindBy(linkText = "Oscars")
     public WebElement oscarsButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement oscarsButtonAssert;
     @FindBy(linkText = "Halloween")
     public WebElement halloweenButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement halloweenButtonAssert;
     @FindBy(linkText = "Hispanic Heritage Month")
     public WebElement hispanicHeritageMonthButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement hispanicHeritageMonthButtonAssert;
     @FindBy(linkText = "MAMI")
     public WebElement mamiButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement mamiButtonAssert;
     @FindBy(linkText = "STARmeter Awards")
     public WebElement sTARmeterAwardsButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement sTARmeterAwardsButtonAssert;
     @FindBy(linkText = "Awards Central")
     public WebElement awardsCentralButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement awardsCentralButtonAssert;
     @FindBy(linkText = "Festival Central")
     public WebElement festivalCentralButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement festivalCentralButtonAssert;
     @FindBy(linkText = "All Events")
     public WebElement allEventsButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement allEventsButtonAssert;
     @FindBy(linkText = "Born Today")
     public WebElement bornTodayButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement bornTodayButtonAssert;
     @FindBy(linkText = "Most Popular Celebs")
     public WebElement mostPopularCelebsButton;
+    @FindBy(css = "[class='ipc-title__text chart-layout-specific-title-text']")
+    public WebElement mostPopularCelebsButtonAssert;
     @FindBy(linkText = "Celebrity News")
     public WebElement celebrityNewsButton;
+    @FindBy(css = "[id='news-card-section']")
+    public WebElement celebrityNewsButtonAssert;
     @FindBy(linkText = "What to Watch")
     public WebElement whattoWatchButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement whattoWatchButtonAssert;
     @FindBy(linkText = "Latest Trailers")
     public WebElement latestTrailersButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement latestTrailersButtonAssert;
     @FindBy(linkText = "IMDb Originals")
     public WebElement iMDbOriginalsButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement iMDbOriginalsButtonAssert;
     @FindBy(linkText = "IMDb Picks")
     public WebElement iMDbPicksButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement iMDbPicksButtonAssert;
     @FindBy(linkText = "IMDb Spotlight")
     public WebElement iMDbSpotlightButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement iMDbSpotlightButtonAssert;
     @FindBy(linkText = "IMDb Podcasts")
     public WebElement iMDbPodcastsButton;
+    @FindBy(css = "[class='ipc-title__text']")
+    public WebElement iMDbPodcastsButtonAssert;
     @FindBy(linkText = "Help Center")
     public WebElement helpCenterButton;
+    @FindBy(css = "[class='a-spacing-base a-size-extra-large']")
+    public WebElement helpCenterButtonAssert;
     @FindBy(linkText = "Contributor Zone")
     public WebElement contributorZoneButton;
+    @FindBy(css = "[class='a-box-inner'] h2")
+    public WebElement contributorZoneButtonAssert;
     @FindBy(linkText = "Polls")
     public WebElement pollsButton;
+    @FindBy(css = "[class='poll homepage']")
+    public WebElement pollsButtonAssert;
+    @FindBy(css = "[class='ipc-watchlist-ribbon ipc-focusable ipc-watchlist-ribbon--m ipc-watchlist-ribbon--baseAlt ipc-watchlist-ribbon--onImage ipc-poster__watchlist-ribbon']")
+    public WebElement addRecentlyWatchlist;
+    @FindBy(css = "[class='ipc-watchlist-ribbon ipc-focusable ipc-watchlist-ribbon--m ipc-watchlist-ribbon--baseAlt ipc-watchlist-ribbon--onImage ipc-watchlist-ribbon--inWatchlist ipc-poster__watchlist-ribbon']")
+    public WebElement removeRecentlyWatchlist;
+
 
 
 
